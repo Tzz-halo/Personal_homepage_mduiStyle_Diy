8eba51a0356ae0bef9e2a6ffd74b0a8c0425b506
Tzz-halo <sikong.top@outlook.com>
2024-05-13 23:41:28 +0800

    第一次提交

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'mdui/mdui.css';
+import './style.css';
+
+import '@mdui/icons/dark-mode.js';
+import 'mdui/components/avatar.js';
+import 'mdui/components/card.js';
+import 'mdui/components/button.js';
+import 'mdui/components/list.js';
+import 'mdui/components/list-item.js';
+import 'mdui/components/list-subheader.js';
+import 'mdui/components/collapse.js';
+import 'mdui/components/collapse-item.js';
+
+createApp(App).mount('#app')
